89fb4bdde0611f7558fbd097ad4d2bb0be1d71b8
Magesh Kuppan <tkmagesh77@gmail.com>
2023-04-11 10:15:10 +0530

    buffered channels - use case

---
+++ 02-concurrency/29-demo.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("Something went wrong :", err)
+		}
+	}()
+	divisor := 0
+	/*
+		quotientCh, errCh := divide(100, divisor)
+		select {
+		case result := <-quotientCh:
+			fmt.Printf("Dividing 100 by %d, quotient = %d\n", divisor, result)
+		case err := <-errCh:
+			fmt.Println(err)
+		}
+	*/
+	timeout := time.After(5 * time.Second)
+	quotientCh, _ := divide(100, divisor)
+	select {
+	case result := <-quotientCh:
+		fmt.Printf("Dividing 100 by %d, quotient = %d\n", divisor, result)
+	case <-timeout:
+		fmt.Println("Timed out...")
+		break
+	}
+}
+
+func divide(x, y int) (<-chan int, <-chan error) {
+	ch := make(chan int)
+	errCh := make(chan error, 1)
+	go func() {
+		defer func() {
+			if err := recover(); err != nil {
+				errCh <- err.(error)
+				fmt.Println("Error sent to error channel")
+			}
+		}()
+		fmt.Println("Performing divide operation.....")
+		time.Sleep(3 * time.Second)
+		ch <- x / y
+	}()
+	return ch, errCh
+
+}
