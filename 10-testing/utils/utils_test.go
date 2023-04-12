4571aee19d92cac50eddb3ef5a0d7f71531ca437
Magesh Kuppan <tkmagesh77@gmail.com>
2023-04-12 16:11:18 +0530

    Unit testing & data driven tests

---
+++ 10-testing/utils/utils_test.go
@@ -0,0 +1,68 @@
+package utils
+
+import (
+	"testing"
+)
+
+/*
+func Test_IsPrime_71(t *testing.T) {
+	// Arrange
+	no := 71
+	expectedResult := true
+
+	//Act
+	actualResult := IsPrime(no)
+
+	//Assert
+	if actualResult != expectedResult {
+
+			// t.Logf("IsPrime(%d) = %t, but got %t\n", no, expectedResult, actualResult)
+			// t.Fail()
+
+		t.Errorf("IsPrime(%d) = %t, but got %t\n", no, expectedResult, actualResult)
+	}
+}
+
+func Test_IsPrime_97(t *testing.T) {
+	// Arrange
+	no := 97
+	expectedResult := true
+
+	//Act
+	actualResult := IsPrime(no)
+
+	//Assert
+	if actualResult != expectedResult {
+
+			// t.Logf("IsPrime(%d) = %t, but got %t\n", no, expectedResult, actualResult)
+			// t.Fail()
+
+		t.Errorf("IsPrime(%d) = %t, but got %t\n", no, expectedResult, actualResult)
+	}
+}
+*/
+
+func Test_IsPrime(t *testing.T) {
+	testData := []struct {
+		name           string
+		no             int
+		actualResult   bool
+		expectedResult bool
+	}{
+		{name: "Testing_IsPrime_71", no: 71, expectedResult: true},
+		{name: "Testing_IsPrime_90", no: 90, expectedResult: false},
+		{name: "Testing_IsPrime_13", no: 13, expectedResult: false},
+		{name: "Testing_IsPrime_91", no: 91, expectedResult: false},
+	}
+
+	for _, td := range testData {
+		t.Run(td.name, func(t *testing.T) {
+			td.actualResult = IsPrime(td.no)
+
+			//Assert
+			if td.actualResult != td.expectedResult {
+				t.Errorf("IsPrime(%d) = %t, but got %t\n", td.no, td.expectedResult, td.actualResult)
+			}
+		})
+	}
+}
